09c60be3925ca553e33cc6dad74ce5f2d8100ea2
Paul <jamespauloconnor@gmail.com>
2016-07-11 21:13:36 -0400

    initial commit

---
+++ 1.index.js
@@ -0,0 +1,12 @@
+	//Set up a localhost
+
+	var app = require('express')();
+	var http = require('http').Server(app);
+
+	app.get('/', function(request, response){
+		response.send('<h1>Hello World</h1>')
+	});
+
+	http.listen(3000, function(){
+		console.log('listening on *:3000')
+	});
