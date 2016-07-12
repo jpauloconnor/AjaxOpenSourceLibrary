29c6c1c2995a2496c6e2b5d34d933287f8c5a3e7
Paul <jamespauloconnor@gmail.com>
2016-07-11 22:42:55 -0400

    Added code by James Handshoe for lesson

--- 1.index.js
+++ 1.index.js
@@ -1,10 +1,9 @@
-	//TO DO Expres 
+	//TODO: Express Overview 
 	
-
-
 
 	//Set up a localhost
 	//Create an instance of express
+	/*
 	var app = require('express')();
 
 	var httpListeningTool = require('http').Server(app);
@@ -15,4 +14,17 @@
 	httpListeningTool.listen(5000, function(){
 		console.log('listening on *:5000')
 	});
-	
+	*/
+
+	var app = require('express')();
+	var http = require('http').Server(app);
+
+	app.get('/', function(req, res){
+		res.sendFile(__dirname + '/1.ajax_example.html');
+	});
+
+	http.listen(5000, function(){
+		console.log('listening on *:5000');
+	});
+
+
