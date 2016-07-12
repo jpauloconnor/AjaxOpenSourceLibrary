48f21fbfa75b666ddf7cb33d75ff18dfdfa83aac
Paul <jamespauloconnor@gmail.com>
2016-07-11 21:43:58 -0400

    update to package.json -> added start and updated instructions

--- 1.index.js
+++ 1.index.js
@@ -1,12 +1,18 @@
+	//TO DO Expres 
+	
+
+
+
 	//Set up a localhost
-
+	//Create an instance of express
 	var app = require('express')();
-	var http = require('http').Server(app);
 
+	var httpListeningTool = require('http').Server(app);
 	app.get('/', function(request, response){
 		response.send('<h1>Hello World</h1>')
 	});
 
-	http.listen(5000, function(){
+	httpListeningTool.listen(5000, function(){
 		console.log('listening on *:5000')
 	});
+	
